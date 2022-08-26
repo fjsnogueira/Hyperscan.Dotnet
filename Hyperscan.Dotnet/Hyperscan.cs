de336b014137680e794a218098d3c63ef60b0f55
Jose Valbuena <josevalbuena@microsoft.com>
2022-08-26 11:53:53 -0400

    adds descriptions

--- Hyperscan.Dotnet/Hyperscan.cs
+++ Hyperscan.Dotnet/Hyperscan.cs
@@ -9,10 +9,10 @@
         internal static extern IntPtr CreateHyperscanEngine();
 
         [DllImport("Hyperscan.Dotnet.Native.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "compile_block_db")]
-        internal static extern int CompileBlockDatabase(IntPtr _hyperscanEngine, string path);
+        internal static extern int CompileBlockDatabase(IntPtr _hyperscanEngine, string patternFile);
 
         [DllImport("Hyperscan.Dotnet.Native.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "scan_single")]
-        internal static extern int ScanSingle(IntPtr _hyperscanEngine, string dataInput);
+        internal static extern int ScanSingle(IntPtr _hyperscanEngine, string data);
 
         [DllImport("Hyperscan.Dotnet.Native.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "clean")]
         internal static extern int Clean(IntPtr _hyperscanEngine);
@@ -24,24 +24,31 @@
 
         private readonly IntPtr _hyperscanEngine;
 
+        /// <summary>
+        /// Initilizes the Hyperscan Engine.
+        /// </summary>
         public Hyperscan()
         {
             _hyperscanEngine = NativeMethods.CreateHyperscanEngine();
         }
 
+        /// <summary>
+        /// Compiles multiple regular expressions into a pattern database (block mode)
+        /// </summary>
+        /// <param name="patternFile">Path to the pattern file</param>
         public void CompileBlockDatabase(string patternFile)
         {
             NativeMethods.CompileBlockDatabase(_hyperscanEngine, patternFile);
         }
 
-        public int ScanSingle(string dataInput)
+        /// <summary>
+        /// The regular expression scanner. Indentifies a match using the compiled patterns.
+        /// </summary>
+        /// <param name="data"> Single data input to be scanned </param>
+        /// <returns> The id of the matched pattern, otherwise -1 </returns>
+        public int ScanSingle(string data)
         {
-            return NativeMethods.ScanSingle(_hyperscanEngine, dataInput);
-        }
-
-        public void Clean()
-        {
-            NativeMethods.Clean(_hyperscanEngine);
+            return NativeMethods.ScanSingle(_hyperscanEngine, data);
         }
 
         private void Dispose(bool disposing)
